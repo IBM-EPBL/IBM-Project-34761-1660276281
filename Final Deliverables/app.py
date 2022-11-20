67e40026bc816ea915cd001a9d7bfa9fed0f6757
19IT089-Shruti-S <74966045+19IT089-Shruti-S@users.noreply.github.com>
2022-11-20 17:37:05 +0530

    Add files via upload

--- Final Deliverables/app.py
+++ Final Deliverables/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
 from twilio.rest import Client
 import math
 
@@ -13,13 +18,16 @@ model = load_model('forests.h5')
 model.make_predict_function()
 
 def predict_label(img_path):
-    i = image.load_img(img_path, target_size=(128,128))
-    i = image.img_to_array(i)/255.0
-    i = i.reshape(1, 128,128,3)
+    i = cv2.imread(img_path)
+    i = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
+    img = Image.open(img_path)
+    img = img.resize((128,128))
+    x = img_to_array(img)
+    x = np.expand_dims(x,axis=0)
     account_sid='AC33e4f23328753859047817ac8815083b'
-    auth_token ='ec85f2a8b7e067400404fd9c0c565797'
+    auth_token ='778340e32fc81ce5fc2f1061ad1791f9'
     client=Client(account_sid,auth_token)
-    pred= model.predict(i)
+    pred= model.predict(x)
     if(pred==[[1.]]):
       message=client.messages \
       .create(
@@ -37,7 +45,11 @@ def predict_label(img_path):
 # routes
 @app.route("/", methods=['GET', 'POST'])
 def main():
-	return render_template("index.html")
+	return render_template("home.html")
+
+@ app.route('/index', methods=['GET', 'POST']) # routes to the index html
+def index():
+    return render_template("index.html")
 
 @app.route("/about")
 def about_page():
